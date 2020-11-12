1682ca3a5ce13c04b173dddc0905eff19f2bcc46
Xiyang Chen <settinghead@gmail.com>
2020-11-12 09:29:27 -0500

    endpoint and include question id

--- ai_notebook_utils/submissions.py
+++ ai_notebook_utils/submissions.py
@@ -1,6 +1,7 @@
 import requests
 
 submission_token: str = None
+ENDPOINT = "https://ai.science"
 
 
 def score_answer(question_id: str, answer):
@@ -10,9 +11,10 @@ def score_answer(question_id: str, answer):
     if not submission_token:
         print("No valid token available. Skipping submission.")
     else:
-        url = 'https://ai.science/api/v1/notebook-submissions'
+        url = f'{ENDPOINT}/api/v1/notebook-submissions'
         myobj = {
             'answer': answer,
+            'questionId': question_id
         }
         result = requests.post(
             url,
@@ -27,10 +29,10 @@ def ensure_token():
     if submission_token is None:
         print(
             "To submit your answer, " +
-            "log in to https://ai.science/my-notebook-token and paste your personal token here: ")
+            f"log in to {ENDPOINT}/my-notebook-token and paste your personal token here: ")
         token_entered = input()
         if token_entered:
-            token_validation_url = f'https://ai.science/api/v1/notebook-tokens/{token_entered}'
+            token_validation_url = f'{ENDPOINT}/api/v1/notebook-tokens/{token_entered}'
             res = requests.get(token_validation_url)
             if res.status_code == 200:
                 submission_token = token_entered
