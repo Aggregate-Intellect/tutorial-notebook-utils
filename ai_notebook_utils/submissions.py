7202b78481499dac3ea324dc9da0c1366dca433c
Xiyang Chen <settinghead@gmail.com>
2020-11-12 00:36:14 -0500

    token prompt and validation

--- ai_notebook_utils/submissions.py
+++ ai_notebook_utils/submissions.py
@@ -1,11 +1,34 @@
 import requests
 
+submission_token: str = None
 
-def submit_notebooks(product_id: str, answer: str):
+
+def score_answer(question_id: str, answer):
     """Sends student submission to retrieve bonus materials."""
-    url = 'https://ai.science/api/v1/notebook-submissions'
-    myobj = {
-        'answer': answer
-    }
-    bonus = requests.post(url, data=myobj)
-    print("Your bonus materials can be found here: ", bonus)
+    ensure_token()
+    global submission_token
+    if not submission_token:
+        print("No valid token available. Skipping submission.")
+    else:
+        url = 'https://ai.science/api/v1/notebook-submissions'
+        myobj = {
+            'answer': answer,
+        }
+        bonus = requests.post(url, data=myobj, headers={
+                              'Authorization', f'Bearer {submission_token}'})
+        print("Your bonus materials can be found here: ", bonus)
+
+
+def ensure_token():
+    global submission_token
+    if submission_token is None:
+        print("To submit your answer, log in to https://ai.science/my-notebook-token and paste your personal token here: ")
+        token_entered = input()
+        if token_entered:
+            token_validation_url = f'https://ai.science/api/v1/notebook-tokens/{token_entered}'
+            res = requests.get(token_validation_url)
+            if res.status_code == 200:
+                submission_token = token_entered
+                return True
+    print("You need to enter a valid token to contine.")
+    return False
