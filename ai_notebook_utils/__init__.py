e282616b896f21d3fba867bf04f46c9e0fc1f90c
Xiyang Chen <settinghead@gmail.com>
2020-11-13 12:47:27 -0500

    fix token submission bug

--- ai_notebook_utils/__init__.py
+++ ai_notebook_utils/__init__.py
@@ -8,24 +8,24 @@ SUBMISSION_API_ENDPOINT = "https://api.ai.science/v1/notebook-submissions"
 
 def score_answer(question_id: str, answer):
     """Sends student submission to retrieve bonus materials."""
-    ensure_token()
-    global submission_token
-    if not submission_token:
-        print("No valid token available. Skipping submission.")
-    else:
-        answer64 = base64.b64encode(answer)
-        url = f'{SUBMISSION_API_ENDPOINT}'
-        payload = {
-            'answer64': answer64,
-            'questionId': question_id,
-            'token': submission_token
-        }
-        result = requests.post(
-            url,
-            data=payload,
-            # headers={'Authorization', f'Bearer {submission_token}'}
-        )
-        print("Submission result: ", result)
+    if ensure_token():
+        global submission_token
+        if not submission_token:
+            print("No valid token available. Skipping submission.")
+        else:
+            answer64 = base64.b64encode(answer)
+            url = f'{SUBMISSION_API_ENDPOINT}'
+            payload = {
+                'answer64': answer64,
+                'questionId': question_id,
+                'token': submission_token
+            }
+            result = requests.post(
+                url,
+                data=payload,
+                # headers={'Authorization', f'Bearer {submission_token}'}
+            )
+            print("Submission result: ", result)
 
 
 def ensure_token():
@@ -41,5 +41,6 @@ def ensure_token():
             if res.status_code == 200:
                 submission_token = token_entered
                 return True
-    print("You need to enter a valid token to contine.")
-    return False
+        print("You need to enter a valid token to contine.")
+        return False
+    return True
